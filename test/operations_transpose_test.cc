fc57ab48b56e650d0b40f67be13a6edf9c4d9019
Colin Le <colintranle123@gmail.com>
2025-10-19 12:07:13 -0700

    update tests for transpose

--- test/operations_transpose_test.cc
+++ test/operations_transpose_test.cc
@@ -10,7 +10,7 @@ TEST(OperationsTransposeTest, SimpleTranspose2D)
     a.set_data(std::vector<int>{1, 2, 3, 4, 5, 6}); // [[1,2,3],[4,5,6]]
 
     auto t = matrix_library::operations::transpose(a);
-    EXPECT_EQ(t.shape(), std::vector<std::size_t>{3, 2});
+    EXPECT_EQ(t.shape(), (std::vector<std::size_t>{3, 2}));
     EXPECT_EQ((t[{0, 0}]), 1);
     EXPECT_EQ((t[{0, 1}]), 4);
     EXPECT_EQ((t[{1, 0}]), 2);
@@ -25,7 +25,7 @@ TEST(OperationsTransposeTest, Transpose1D)
     v.set_data(std::vector<int>{7, 8, 9});
 
     auto t = matrix_library::operations::transpose(v);
-    EXPECT_EQ(t.shape(), std::vector<std::size_t>{3, 1});
+    EXPECT_EQ(t.shape(), (std::vector<std::size_t>{3, 1}));
     EXPECT_EQ((t[{0, 0}]), 7);
     EXPECT_EQ((t[{1, 0}]), 8);
     EXPECT_EQ((t[{2, 0}]), 9);
